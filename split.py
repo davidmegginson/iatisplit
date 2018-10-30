f1d3bc7f42773e55da8d2296316b1092fc47a5a3
David Megginson <david.megginson@gmail.com>
2018-10-30 15:07:49 +0100

    More logic in PoC split

--- split.py
+++ split.py
@@ -1,7 +1,17 @@
 import xml.dom.pulldom
 import sys
 
+LIMIT = 5
+
+counter = 0
+
 doc = xml.dom.pulldom.parse(sys.argv[1])
 
 for event, node in doc:
-    print(event, node.toxml())
+    if event == xml.dom.pulldom.START_ELEMENT and node.tagName == 'iati-activity':
+        if counter < LIMIT:
+            doc.expandNode(node)
+            print(node.toprettyxml(indent='  '))
+        else:
+            break
+        counter += 1
