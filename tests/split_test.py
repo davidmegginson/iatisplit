777160e456f259978c050d034ac680fe76579020
David Megginson <david.megginson@gmail.com>
2018-11-12 17:09:19 -0500

    First actual unit tests.

--- tests/split_test.py
+++ tests/split_test.py
@@ -11,7 +11,21 @@ import unittest
 
 import iatisplit.split
 
+import os
+import xml.dom.minidom
+
 class TestSplit(unittest.TestCase):
 
-    def test_true(self):
-        self.assertTrue(True)
+    def test_get_text_simple(self):
+        dom = xml.dom.minidom.parseString("<narrative>abcde</narrative>")
+        self.assertEqual("abcde", iatisplit.split.get_text(dom.getElementsByTagName("narrative")[0]))
+
+    def test_get_text_multiple_nodes(self):
+        dom = xml.dom.minidom.parseString("<narrative>abc<foo/>de</narrative>")
+        self.assertEqual("abcde", iatisplit.split.get_text(dom.getElementsByTagName("narrative")[0]))
+
+def read_xml(filename):
+    """Read a test XML file and return a miniDOM"""
+    path = os.path.join(os.path.dirname(__file__), "files", filename)
+    return xml.dom.minidom.parse(path)
+
